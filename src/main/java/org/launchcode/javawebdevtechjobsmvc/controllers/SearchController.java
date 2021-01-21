1db612279e8f8631b9f570a1bc315bfa1d46f877
Rexzellius <katlynmmills@gmail.com>
2021-01-20 19:13:31 -0600

    Included Super Bonus mission

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.columnChoices;
@@ -15,7 +16,7 @@ import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.co
  */
 @Controller
 @RequestMapping("search")
-public class SearchController {
+public class SearchController extends TechJobsController {
 
     @RequestMapping(value = "")
     public String search(Model model) {
@@ -24,5 +25,12 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
-
+    @RequestMapping(value = "results", method = RequestMethod.POST)
+    public String displaySearchResults(Model model, String searchType, String searchTerm) {
+        model.addAttribute("currentSelection", searchType);
+        ArrayList<Job> jobs = searchType.equals("all") ? JobData.findAll() : JobData.findByColumnAndValue(searchType, searchTerm);
+        model.addAttribute("jobs", jobs);
+        model.addAttribute("title", "Search Results");
+        return "search";
+    }
 }
